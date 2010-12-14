6055ec94a10d6ddd30f2d6497b61dd05b3d4fee0
BenStoever <stoever@bioinfweb.info>
2010-12-14 13:56:42 +0000

    XSD for http://bioinfweb.info/xmlns/SQLConnection added. Tags in SQLXMLConstants.java changed. WARNING: Applications using the previous tags must be updated. (Should only be the BioInfWeb CMS.)

--- main/src/info/webinsel/util/servlet/sqlproperties/SQLXMLConstants.java
+++ main/src/info/webinsel/util/servlet/sqlproperties/SQLXMLConstants.java
@@ -1,16 +1,27 @@
 package info.webinsel.util.servlet.sqlproperties;
 
 
+import info.webinsel.util.io.FormatVersion;
+
 import javax.xml.namespace.QName;
 
 
 
+/**
+ * XML constants for the namespace <i>http://bioinfweb.info/xmlns/SQLConnection</i>.
+ * 
+ * @author Ben St&ouml;ver
+ */
 public interface SQLXMLConstants {
-  public static final QName TAG_DB = new QName("Database");
-  public static final QName TAG_DB_HOST = new QName("Host");
-  public static final QName TAG_DB_NAME = new QName("Name");
-  public static final QName TAG_DB_USER = new QName("User");
-  public static final QName TAG_DB_PASSWORD = new QName("Password");
-  public static final QName TAG_DB_DRIVER = new QName("Driver");
-  public static final QName TAG_TABLE_PREFIX = new QName("TablePrefix");
+  public static final String NAMESPACE_URI = "http://bioinfweb.info/xmlns/SQLConnection";
+  public static final FormatVersion VERSION = new FormatVersion(1, 0);
+  public static final String FULL_SCHEMA_LOCATION = NAMESPACE_URI + " " + NAMESPACE_URI + "/" + VERSION + ".xsd";
+  
+  public static final QName TAG_DB = new QName(NAMESPACE_URI, "database");
+  public static final QName TAG_DB_HOST = new QName(NAMESPACE_URI, "host");
+  public static final QName TAG_DB_NAME = new QName(NAMESPACE_URI, "name");
+  public static final QName TAG_DB_USER = new QName(NAMESPACE_URI, "user");
+  public static final QName TAG_DB_PASSWORD = new QName(NAMESPACE_URI, "password");
+  public static final QName TAG_DB_DRIVER = new QName(NAMESPACE_URI, "driver");
+  public static final QName TAG_TABLE_PREFIX = new QName(NAMESPACE_URI, "table-prefix");
 }
