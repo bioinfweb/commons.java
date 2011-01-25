10913a3bbe19b9fa79cf886555993332a44c374b
BenStoever <stoever@bioinfweb.info>
2011-01-25 14:56:10 +0000

    Tests for min and max functions added of Math2 added.

--- test/src/info/webinsel/util/Math2Test.java
+++ test/src/info/webinsel/util/Math2Test.java
@@ -1,6 +1,8 @@
 package info.webinsel.util;
 
 
+import java.util.Arrays;
+
 import org.junit.* ;
 
 
@@ -22,4 +24,60 @@ public class Math2Test {
   public void test_sum1ToN_exception() {
 		Math2.sum1ToN(-1);
 	}
+	
+	
+	@Test
+  public void test_minInt() {
+		assertEquals(0, Math2.minInt(100, 24, 0, 23, 36, 78));
+		assertEquals(0, Math2.minInt(0, 23, 36, 78));
+	}
+	
+	
+	@Test
+  public void test_minLong() {
+		assertEquals(0, Math2.minLong(100, 24, 0, 23, 36, 78));
+		assertEquals(0, Math2.minLong(0, 23, 36, 78));
+	}
+	
+	
+	@Test
+  public void test_minFloat() {
+		assertEquals(0f, Math2.minFloat(100f, 24f, 0f, 23f, 36f, 78f), 0f);
+		assertEquals(0f, Math2.minFloat(0f, 23f, 36f, 78f), 0f);
+	}
+	
+	
+	@Test
+  public void test_minDouble() {
+		assertEquals(0.0, Math2.minDouble(100.0, 24.0, 0.0, 23.0, 36.0, 78.0), 0.0);
+		assertEquals(0.0, Math2.minDouble(0.0, 23.0, 36.0, 78.0), 0.0);
+	}
+	
+	
+	@Test
+  public void test_maxInt() {
+		assertEquals(100, Math2.maxInt(0, 24, 100, 23, 36, 78));
+		assertEquals(100, Math2.maxInt(100, 23, 36, 78));
+	}	
+	
+	
+	@Test
+  public void test_maxLong() {
+		assertEquals(100, Math2.maxLong(0, 24, 100, 23, 36, 78));
+		assertEquals(100, Math2.maxLong(100, 23, 36, 78));
+	}	
+	
+	
+	@Test
+  public void test_maxFloat() {
+		assertEquals(100, Math2.maxFloat(0, 24, 100, 23, 36, 78), 0f);
+		assertEquals(100, Math2.maxFloat(100, 23, 36, 78), 0f);
+	}	
+	
+	
+	@Test
+  public void test_maxDouble() {
+		assertEquals(100, Math2.maxDouble(0, 24, 100, 23, 36, 78), 0.0);
+		assertEquals(100, Math2.maxDouble(100, 23, 36, 78), 0.0);
+	}	
 }
