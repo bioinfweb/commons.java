9556ee20b77cd11a8044a26b0d1c1cb39b58e786
BenStoever <stoever@bioinfweb.info>
2012-01-11 10:02:26 +0000

    Tests for methods in StringUtils extended.

--- test/src/info/webinsel/util/StringUtilsTest.java
+++ test/src/info/webinsel/util/StringUtilsTest.java
@@ -12,17 +12,23 @@ public class StringUtilsTest {
   @Test
   public void test_firstCharToLowerCase() {
   	assertEquals("content", StringUtils.firstCharToLowerCase("Content"));
+  	assertEquals("", StringUtils.firstCharToLowerCase(""));
+  	assertEquals("c", StringUtils.firstCharToLowerCase("C"));
   }
   
   
   @Test
   public void test_firstCharToUpperCase() {
   	assertEquals("Content", StringUtils.firstCharToUpperCase("content"));
+  	assertEquals("", StringUtils.firstCharToUpperCase(""));
+  	assertEquals("C", StringUtils.firstCharToUpperCase("c"));
   }
   
   
   @Test
   public void test_convertCamelCase() {
   	assertEquals("an-example-keyword", StringUtils.convertCamelCase("anExampleKeyword", "-"));
+  	assertEquals("", StringUtils.convertCamelCase("", "-"));
+  	assertEquals("a", StringUtils.convertCamelCase("A", "-"));
   }
 }
