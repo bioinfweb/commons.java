5bb803edb90c747e6f7ac8422337d18ccd80179a
BenStoever <stoever@bioinfweb.info>
2012-12-14 16:13:56 +0000

    Bugfix in SequenceIntervalList.iterator(). (Iterators of empty lists work now.)

--- main/src/info/webinsel/util/collections/SequenceIntervalList.java
+++ main/src/info/webinsel/util/collections/SequenceIntervalList.java
@@ -351,11 +351,13 @@ public class SequenceIntervalList<E> implements Collection<E> {
 					
 					private boolean secureFilledListIterator() {
 						if (((listIterator == null) || !listIterator.hasNext()) && intervalIterator.hasNext()) {
-							do {  // Search next filled list
-								listIterator = intervalIterator.next().getStartList().iterator();
-							} while (!listIterator.hasNext() && intervalIterator.hasNext());
+							if (intervalIterator.hasNext()) {  // This test is necessary if the intervalIterator is empty from beginning on. 
+								do {  // Search next filled list
+									listIterator = intervalIterator.next().getStartList().iterator();
+								} while (!listIterator.hasNext() && intervalIterator.hasNext());
+							}
 						}
-						return (listIterator == null) || listIterator.hasNext();  // Returns false of the end of intervalIterator was reached and the last listIterator contains no more elements
+						return (listIterator != null) && listIterator.hasNext();  // Returns false of the end of intervalIterator was reached and the last listIterator contains no more elements
 					}
 					
 					
@@ -368,9 +370,6 @@ public class SequenceIntervalList<E> implements Collection<E> {
 					@Override
 					public E next() {
 						secureFilledListIterator();
-						if (listIterator == null) {
-							
-						}
 						return listIterator.next();
 					}
 					
