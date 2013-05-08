e4a72cc2d5ebabb03b08a4d89e9f31da22d36255
BenStoever <stoever@bioinfweb.info>
2013-05-08 14:43:04 +0000

    IS_64_BIT_JRE property added to ProgramMainClass.

--- main/src/info/webinsel/util/ProgramMainClass.java
+++ main/src/info/webinsel/util/ProgramMainClass.java
@@ -11,6 +11,9 @@ public class ProgramMainClass {
 	public static final boolean IS_MAC = 
 		  System.getProperty("os.name").toLowerCase().startsWith("mac");
 	
+	public static final boolean IS_64_BIT_JRE = 
+			System.getProperty("os.arch").contains("64");  
+
 
 	private ApplicationVersion version;
 	private Preferences preferences;
