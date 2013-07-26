dc2e24ba3144f36db4868d25e8325c4e7ce239b8
BenStoever <stoever@bioinfweb.info>
2013-07-26 15:10:09 +0000

    ProgressMonitor classes added.

---
+++ main/src/info/webinsel/util/progress/AbstractProgressMonitor.java
@@ -0,0 +1,14 @@
+package info.webinsel.util.progress;
+
+
+
+/**
+ * Basic implementation of the {@link ProgressMonitor} interface.
+ * 
+ * @author Ben St&ouml;ver
+ */
+public abstract class AbstractProgressMonitor implements ProgressMonitor {
+	public void addToProgressValue(double addend) {
+		setProgressValue(getProgressValue() + addend);
+	}
+}
