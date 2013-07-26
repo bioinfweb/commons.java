5b31d3a74b192081337211d435d310a78e8b7b23
BenStoever <stoever@bioinfweb.info>
2013-07-26 16:01:47 +0000

    MultipleApplicationLoggersAdapter added.

---
+++ main/src/info/webinsel/util/log/MultipleApplicationLoggersAdapter.java
@@ -0,0 +1,87 @@
+package info.webinsel.util.log;
+
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Vector;
+
+
+
+/**
+ * This implementation of {@link ApplicationLogger} delegates to a set of other classes implementing
+ * this interface. It can be used of several different implementations of {@link ApplicationLogger} 
+ * should monitor a process at the same time.
+ * 
+ * @author Ben St&ouml;ver
+ */
+public class MultipleApplicationLoggersAdapter implements ApplicationLogger {
+	private Collection<ApplicationLogger> loggers = new Vector<ApplicationLogger>(8);
+	
+
+	public Collection<ApplicationLogger> getLoggers() {
+		return loggers;
+	}
+
+
+	@Override
+	public void addMessage(ApplicationLoggerMessage message) {
+		Iterator<ApplicationLogger> iterator = loggers.iterator();
+		while (iterator.hasNext()) {
+			iterator.next().addMessage(message);
+		}
+	}
+	
+
+	@Override
+	public void addMessage(String message) {
+		Iterator<ApplicationLogger> iterator = loggers.iterator();
+		while (iterator.hasNext()) {
+			iterator.next().addMessage(message);
+		}
+	}
+
+	
+	@Override
+	public void addMessage(String message, int helpCode) {
+		Iterator<ApplicationLogger> iterator = loggers.iterator();
+		while (iterator.hasNext()) {
+			iterator.next().addMessage(message, helpCode);
+		}
+	}
+
+	
+	@Override
+	public void addWarning(String message) {
+		Iterator<ApplicationLogger> iterator = loggers.iterator();
+		while (iterator.hasNext()) {
+			iterator.next().addWarning(message);
+		}
+	}
+
+	
+	@Override
+	public void addWarning(String message, int helpCode) {
+		Iterator<ApplicationLogger> iterator = loggers.iterator();
+		while (iterator.hasNext()) {
+			iterator.next().addWarning(message, helpCode);
+		}
+	}
+	
+
+	@Override
+	public void addError(String message) {
+		Iterator<ApplicationLogger> iterator = loggers.iterator();
+		while (iterator.hasNext()) {
+			iterator.next().addError(message);
+		}
+	}
+
+	
+	@Override
+	public void addError(String message, int helpCode) {
+		Iterator<ApplicationLogger> iterator = loggers.iterator();
+		while (iterator.hasNext()) {
+			iterator.next().addError(message, helpCode);
+		}
+	}
+}
