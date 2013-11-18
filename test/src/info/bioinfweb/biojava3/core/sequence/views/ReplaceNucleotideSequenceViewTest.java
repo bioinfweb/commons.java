9e743eb9c02ad2e406320cc4fd9ad08b6037745b
BenStoever <stoever@bioinfweb.info>
2013-11-18 15:14:51 +0000

    ReplaceNucleotideSequenceViewTest adjusted to recent changes. UpperCaseSequenceViewTest added.

--- test/src/info/bioinfweb/biojava3/core/sequence/views/ReplaceNucleotideSequenceViewTest.java
+++ test/src/info/bioinfweb/biojava3/core/sequence/views/ReplaceNucleotideSequenceViewTest.java
@@ -1,9 +1,10 @@
 package info.bioinfweb.biojava3.core.sequence.views;
 
 
-import static org.junit.Assert.assertEquals;
-import info.bioinfweb.biojava3.core.sequence.compound.AlignmentAmbiguityDNACompoundSet;
+import static org.junit.Assert.*;
 
+
+import info.bioinfweb.biojava3.core.sequence.compound.AlignmentAmbiguityDNACompoundSet;
 
 import org.biojava3.core.sequence.DNASequence;
 import org.junit.Test;
@@ -16,7 +17,7 @@ public class ReplaceNucleotideSequenceViewTest {
   	ReplaceNucleotideSequenceView view = new ReplaceNucleotideSequenceView(
   			new DNASequence("AT?TCG??TGA", AlignmentAmbiguityDNACompoundSet.getAlignmentAmbiguityDNACompoundSet()), 
   			ReplaceNucleotideSequenceView.AMBIGUITY_UNKNOWN_RNA_TO_N_DNA_MAP);
-  	assertEquals(3, view.countReplacedCompounds());
+  	assertEquals(3, view.countChangedCompounds());
   }
   
   
@@ -25,6 +26,6 @@ public class ReplaceNucleotideSequenceViewTest {
   	ReplaceNucleotideSequenceView view = new ReplaceNucleotideSequenceView(
   			new DNASequence("????AT?TCG??TGA???", AlignmentAmbiguityDNACompoundSet.getAlignmentAmbiguityDNACompoundSet()), 
   			ReplaceNucleotideSequenceView.AMBIGUITY_UNKNOWN_RNA_TO_N_DNA_MAP, 5, 15);
-  	assertEquals(3, view.countReplacedCompounds());
+  	assertEquals(3, view.countChangedCompounds());
   }
 }
---
+++ test/src/info/bioinfweb/biojava3/core/sequence/views/UpperCaseSequenceViewTest.java
@@ -0,0 +1,41 @@
+package info.bioinfweb.biojava3.core.sequence.views;
+
+
+import static org.junit.Assert.*;
+
+
+import org.biojava3.core.sequence.DNASequence;
+import org.biojava3.core.sequence.ProteinSequence;
+import org.biojava3.core.sequence.compound.AminoAcidCompound;
+import org.biojava3.core.sequence.compound.AminoAcidCompoundSet;
+import org.biojava3.core.sequence.compound.DNACompoundSet;
+import org.biojava3.core.sequence.compound.NucleotideCompound;
+import org.junit.Test;
+
+
+
+public class UpperCaseSequenceViewTest {
+  @Test
+  public void testWithDNA() {
+  	String sequence = "ATcGtaCgT";
+  	UpperCaseSequenceView<NucleotideCompound> view = 
+  			new UpperCaseSequenceView<NucleotideCompound>(new DNASequence(sequence), DNACompoundSet.getDNACompoundSet());
+  	
+  	assertEquals(sequence.toUpperCase(), view.getSequenceAsString());
+  	assertEquals(4, view.countChangedCompounds());
+  }
+
+
+  // Test with proteins not necessary because AminoAcidCompoundSet does not include lower case characters anyway.
+//  @Test
+//  public void testWithProtein() {
+//  	String sequence = "TQRnCLGgarGH";
+//  	UpperCaseSequenceView<AminoAcidCompound> view = 
+//  			new UpperCaseSequenceView<AminoAcidCompound>(new ProteinSequence(sequence), 
+//  					AminoAcidCompoundSet.getAminoAcidCompoundSet());
+//  	
+//  	System.out.println(view.getSequenceAsString());
+//  	assertEquals(sequence.toUpperCase(), view.getSequenceAsString());
+//  	assertEquals(4, view.countChangedCompounds());
+//  }
+}
