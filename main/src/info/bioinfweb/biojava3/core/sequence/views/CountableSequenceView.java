724af0066a4537c4f1a9c50d7ffdcb3cf5dc7ea5
BenStoever <stoever@bioinfweb.info>
2014-01-23 16:26:29 +0000

    ConcatenatedSequenceView added.

--- main/src/info/bioinfweb/biojava3/core/sequence/views/CountableSequenceView.java
+++ main/src/info/bioinfweb/biojava3/core/sequence/views/CountableSequenceView.java
@@ -6,7 +6,7 @@ package info.bioinfweb.biojava3.core.sequence.views;
  * Should be implemented by all sequence views that e.g. replace, delete or add a certain number of compounds to/from
  * the underlying sequence.
  *  
- * @author BenStoever
+ * @author Ben St&ouml;ver
  */
 public interface CountableSequenceView {
 	public int countChangedCompounds();
