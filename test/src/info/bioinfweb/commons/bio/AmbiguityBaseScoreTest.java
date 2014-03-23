b6cb176459e5768e441bbc6d303c58a58abd1d55
BenStoever <stoever@bioinfweb.info>
2014-03-23 18:22:36 +0000

    Test cases for bioinfweb.commons.bio moved from old package. Test case for AmbiguityBaseScore.rescale() added.

--- test/src/info/bioinfweb/commons/bio/AmbiguityBaseScoreTest.java
+++ test/src/info/bioinfweb/commons/bio/AmbiguityBaseScoreTest.java
@@ -1,4 +1,4 @@
-package info.bioinfweb.util;
+package info.bioinfweb.commons.bio;
 
 
 import static org.junit.Assert.*;
@@ -15,5 +15,15 @@ public class AmbiguityBaseScoreTest {
     assertTrue(new AmbiguityBaseScore(1, 0, 0, 0).compareTo(new AmbiguityBaseScore(0, 1, 1, 1)) > 0);
     assertTrue(new AmbiguityBaseScore(0, 1, 1, 1).compareTo(new AmbiguityBaseScore(0, 1, 0, 1)) > 0);
     assertTrue(new AmbiguityBaseScore(0, 0, 0, 0.25).compareTo(new AmbiguityBaseScore(0, 0, 0, 0.75)) < 0);
+	}
+	
+	
+	@Test
+	public void test_rescale() {
+		AmbiguityBaseScore score = new AmbiguityBaseScore(1, 0, 1, 0).rescale(1);
+		assertEquals(score.getAdeninScore(), .5, 0.0);
+		assertEquals(score.getThyminScore(), 0.0, 0.0);
+		assertEquals(score.getCytosinScore(), .5, 0.0);
+		assertEquals(score.getGuaninScore(), 0.0, 0.0);
 	}
 }
