1a1c4b4ef9bd67fffafe5df9076126590aecb6c6
BenStoever <stoever@bioinfweb.info>
2014-05-26 13:29:55 +0000

    PackedSignedIntegerArrayListTest removed due to recent changes in main project. Additional test cases added to PackedIntegerArrayListTest.

--- test/src/info/bioinfweb/commons/collections/PackedIntegerArrayListTest.java
+++ test/src/info/bioinfweb/commons/collections/PackedIntegerArrayListTest.java
@@ -1,7 +1,7 @@
 /*
- * LibrAlign - A GUI library for displaying and editing multiple sequence alignments and attached data
- * Copyright (C) 2014  Ben Stöver
- * <http://bioinfweb.info/LibrAlign>
+ * bioinfweb.commons - Shared components of bioinfweb projects made available in a Java library
+ * Copyright (C) 2010-2014  Ben Stöver
+ * <http://commons.bioinfweb.info/Java>
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -45,7 +45,7 @@ public class PackedIntegerArrayListTest {
   public void testInsertRange(final long intialListSize, final long insertIndex, final long insertionLength, 
   		final int elementLength) {
   	
-  	PackedIntegerArrayList list = new PackedIntegerArrayList(elementLength, intialListSize);
+  	PackedIntegerArrayList list = new PackedIntegerArrayList(elementLength, 0, intialListSize);
 		for (long i = 0; i < intialListSize; i++) {
 			list.add(i);
 		}
@@ -72,7 +72,7 @@ public class PackedIntegerArrayListTest {
   		//System.out.println("Start " + insertIndex);
     	for (int insertionLength = 0; insertionLength < 64; insertionLength++) {
     		//System.out.println("  length " + insertionLength);
-    		for (int elementLength = 6; elementLength <= 64; elementLength++) {
+    		for (int elementLength = 6; elementLength < 64; elementLength++) {
         	//System.out.print(elementLength + " ");
         	testInsertRange(64, insertIndex, insertionLength, elementLength);
 				}
@@ -86,7 +86,7 @@ public class PackedIntegerArrayListTest {
   private void testRemoveRange(final long intialListSize, final long removeIndex, final long removeLength, 
   		final int elementLength) {
   	
-  	PackedIntegerArrayList list = new PackedIntegerArrayList(elementLength, intialListSize);
+  	PackedIntegerArrayList list = new PackedIntegerArrayList(elementLength, 0, intialListSize);
 		for (long i = 0; i < intialListSize; i++) {
 			list.add(i);
 		}
@@ -114,7 +114,7 @@ public class PackedIntegerArrayListTest {
   		//System.out.println("Start " + removeStart);
     	for (int removeLength = 0; removeLength <= maxRemove; removeLength++) {
     		//System.out.println("  length " + removeLength);
-    		for (int elementLength = 6; elementLength <= 64; elementLength++) {
+    		for (int elementLength = 6; elementLength < 64; elementLength++) {
         	//System.out.print(elementLength + " ");
         	testRemoveRange(length, removeStart, removeLength, elementLength);
 				}
@@ -123,6 +123,128 @@ public class PackedIntegerArrayListTest {
     	//System.out.println();
 		}
   }
+  
+  
+	@Test
+	public void test_get_set_signed() {
+		final int minValue = -10;
+		final int size = 128;
+		
+		for (int bitsPerValue = 7; bitsPerValue < 64; bitsPerValue++) {
+			PackedIntegerArrayList list = new PackedIntegerArrayList(bitsPerValue, minValue, size);
+			for (int i = minValue; i < size + minValue; i++) {
+				list.add(i);
+			}
+			
+			for (int i = 0; i < size; i++) {
+				assertEquals(i + minValue, list.get(i));
+			}
+		}
+	}
+	
+	
+	@Test
+	public void test_getMaxValue() {
+  	PackedIntegerArrayList list = new PackedIntegerArrayList(15,-10, 5);
+  	assertEquals(-10, list.getMinValue());
+  	assertEquals(32757, list.getMaxValue());
+	}
+
+	
+	@Test(expected=IndexOutOfBoundsException.class)
+  public void test_add_lowIndexException() {
+  	PackedIntegerArrayList list = new PackedIntegerArrayList(7, 0, 5);
+  	list.add(-1, 18);
+  }
+	
+  
+  @Test(expected=IndexOutOfBoundsException.class)
+  public void test_add_highIndexException() {
+  	PackedIntegerArrayList list = new PackedIntegerArrayList(7, 0, 5);
+  	list.add(1, 18);
+  }
+	
+  
+  @Test(expected=IndexOutOfBoundsException.class)
+  public void test_get_lowIndexException() {
+  	PackedIntegerArrayList list = new PackedIntegerArrayList(7, 0, 5);
+  	list.get(-1);
+  }
+	
+  
+  @Test(expected=IndexOutOfBoundsException.class)
+  public void test_get_highIndexException() {
+  	PackedIntegerArrayList list = new PackedIntegerArrayList(7, 0, 5);
+  	list.add(18);
+  	list.get(1);
+  }
+	
+  
+  @Test(expected=IndexOutOfBoundsException.class)
+  public void test_get_highIndexException2() {
+  	PackedIntegerArrayList list = new PackedIntegerArrayList(7, 0, 5);
+  	list.get(0);
+  }
+	
+  
+  @Test(expected=IndexOutOfBoundsException.class)
+  public void test_set_lowIndexException() {
+  	PackedIntegerArrayList list = new PackedIntegerArrayList(7, 0, 5);
+  	list.set(-1, 18);
+  }
+	
+  
+  @Test(expected=IndexOutOfBoundsException.class)
+  public void test_set_highIndexException() {
+  	PackedIntegerArrayList list = new PackedIntegerArrayList(7, 0, 5);
+  	list.set(0, 18);
+  }
+	
+  
+  @Test(expected=IllegalArgumentException.class)
+  public void test_set_lowValueException() {
+  	PackedIntegerArrayList list = new PackedIntegerArrayList(7, -10, 5);
+  	list.add(0);
+  	list.set(0, -11);
+  }
+	
+  
+  @Test(expected=IllegalArgumentException.class)
+  public void test_set_highValueException() {
+  	PackedIntegerArrayList list = new PackedIntegerArrayList(7, -10, 5);
+  	list.add(0);
+  	list.set(0, 118);
+  }
+	
+  
+  @Test(expected=IllegalArgumentException.class)
+  public void test_add_lowValueException() {
+  	PackedIntegerArrayList list = new PackedIntegerArrayList(7, -10, 5);
+  	list.add(-11);
+  }
+	
+  
+  @Test(expected=IllegalArgumentException.class)
+  public void test_add_highValueException() {
+  	PackedIntegerArrayList list = new PackedIntegerArrayList(7, -10, 5);
+  	list.add(118);
+  }
+	
+  
+  @Test(expected=IllegalArgumentException.class)
+  public void test_add2_lowValueException() {
+  	PackedIntegerArrayList list = new PackedIntegerArrayList(7, -10, 5);
+  	list.add(0);
+  	list.add(1, -11);
+  }
+	
+  
+  @Test(expected=IllegalArgumentException.class)
+  public void test_add2_highValueException() {
+  	PackedIntegerArrayList list = new PackedIntegerArrayList(7, -10, 5);
+  	list.add(0);
+  	list.add(1, 118);
+  }
 	
   
   public static void main(String[] args) {
@@ -130,7 +252,7 @@ public class PackedIntegerArrayListTest {
   	System.out.println(TestTools.toBinaryRepresentation(2001l >>> 63));
   	System.out.println(TestTools.toBinaryRepresentation(2001l >>> 64));  // Does nothing
   	
-  	PackedIntegerArrayList list = new PackedIntegerArrayList(6, 100);
+  	PackedIntegerArrayList list = new PackedIntegerArrayList(6, 0, 100);
 		list.set(56, 8);
 		list.set(60, 63);
 		list.set(61, 64);
