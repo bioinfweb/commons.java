f6cb23aa0401d3bc4f8c1f2d2f7ac36165ced24a
BenStoever <stoever@bioinfweb.info>
2014-05-27 08:48:57 +0000

    License information edited.

--- main/core/info/bioinfweb/commons/collections/PackedIntegerArrayList.java
+++ main/core/info/bioinfweb/commons/collections/PackedIntegerArrayList.java
@@ -1,20 +1,39 @@
 /*
  * bioinfweb.commons - Shared components of bioinfweb projects made available in a Java library
  * Copyright (C) 2014  Ben Stöver
- * <http://bioinfweb.info/Commons>
+ * <http://commons.bioinfweb.info/Java>
  * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *   This file is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
  * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ *   This file is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *   
+ * This file incorporates work covered by the following copyright and  
+ * permission notice (Mainly but not exclusively the implementations of
+ * the get() and set() methods are based on the Apache Lucene source code.):
+ * 
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the APACHE-NOTICE file distributed with
+ *   this work for additional information regarding copyright ownership.
+ *   The ASF licenses this file to You under the Apache License, Version 2.0
+ *   (the "License"); you may not use this file except in compliance with
+ *   the License.  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 package info.bioinfweb.commons.collections;
 
