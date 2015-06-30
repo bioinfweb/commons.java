7c8a671b1d09b5ede2ba3decc3b3c5927fd5aae5
BenStoever <stoever@bioinfweb.info>
2015-06-30 17:22:42 +0000

    TIC module removed from repository. (TIC version 2.x will not be part of bioinfweb.commons anymore and is now available as a bioinfweb top level project instead.)

--- main/tic/info/bioinfweb/commons/tic/input/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Contains classes providing adapters between Swing and SWT mouse and key event related classes.
- * 
- * @author Ben St&ouml;ver
- */
-package info.bioinfweb.commons.tic.input;
