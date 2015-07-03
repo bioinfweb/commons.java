3da4ecfd4b410e22ba05c641f250ee892c2a7a30
BenStoever <stoever@bioinfweb.info>
2015-07-03 18:52:42 +0000

    Moved parts removed from oldcombined project.

--- main/info.bioinfweb.commons.oldcombined/core/info/bioinfweb/commons/collections/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Contains extensions to the Java Collections API made for bioinfweb projects that can be of general use.
- * 
- * @author Ben St&ouml;ver
- * @since 1.0.0
- */
-package info.bioinfweb.commons.collections;
--- main/info.bioinfweb.commons.oldcombined/experimental/info/bioinfweb/commons/bio/alignment/pairwise/PairwiseAligner.java
+++
@@ -1,7 +0,0 @@
-package info.bioinfweb.commons.bio.alignment.pairwise;
-
-
-
-public interface PairwiseAligner {
-	public CharSequence[] align(CharSequence seqA, CharSequence seqB);
-}
