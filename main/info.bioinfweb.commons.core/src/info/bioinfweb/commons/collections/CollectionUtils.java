f8880ef8a6f021f5c879713544f6c376c83dff66
BenStoever <stoever@bioinfweb.info>
2016-01-08 14:33:47 +0000

    Copyright information in core project updated.

--- main/info.bioinfweb.commons.core/src/info/bioinfweb/commons/collections/CollectionUtils.java
+++ main/info.bioinfweb.commons.core/src/info/bioinfweb/commons/collections/CollectionUtils.java
@@ -1,6 +1,6 @@
 /*
  * bioinfweb.commons - Shared components of bioinfweb projects made available in a Java library
- * Copyright (C) 2014  Ben Stöver
+ * Copyright (C) 2008-2016  Ben Stöver
  * <http://commons.bioinfweb.info/Java>
  * 
  * This file is free software: you can redistribute it and/or modify
