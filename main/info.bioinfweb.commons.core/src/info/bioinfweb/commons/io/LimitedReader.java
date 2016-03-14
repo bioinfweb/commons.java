e618d29dcd3459527a2b62e1ff3e45a37612d87e
BenStoever <stoever@bioinfweb.info>
2016-03-14 13:00:03 +0000

    Constructor exceptions added to LimitedReader. Additional read character removed from LimitedReader.

--- main/info.bioinfweb.commons.core/src/info/bioinfweb/commons/io/LimitedReader.java
+++ main/info.bioinfweb.commons.core/src/info/bioinfweb/commons/io/LimitedReader.java
@@ -32,9 +32,6 @@ import java.io.Reader;
  * In contrast to many similar classes, the focus of this class is not on limiting the number of characters to be returned by this
  * reader, but to make sure that not more characters than specified can be read from the underlying reader. It can e.g. be used
  * to ensure that reading beyond the read ahead limit of a call of {@link #mark(int)} is not possible.
- * <p>
- * Note that for testing the end of the underlying stream one character more needs to be read from the decorated reader than can
- * be returned by this instance. Therefore one character less than the limit specified in the constructor will at most be returned. 
  * 
  * @author Ben St&ouml;ver
  * @since 2.0.0
@@ -42,29 +39,35 @@ import java.io.Reader;
 public class LimitedReader extends Reader {
 	//TODO Additional methods could be directly delegated to use possible performance increasing implementations of the decorated reader.
 	
-	private Reader underlyingReader;
+	private Reader decoratedReader;
 	private long limit;
 	private long position = 0;
-	private long markPosition = 0; 
+	private long markPosition = 0;
 	
 	
 	/**
 	 * Creates a new instance of this class.
 	 * 
-	 * @param underlyingReader the reader to be decorated
-	 * @param limit the maximum number of characters that shall be read from the decorated reader (Note that this reader will return
-	 *        at most {@code limit - 1} characters.)
+	 * @param deoratedReader the reader to be decorated
+	 * @param limit the maximum number of characters that shall be read from the decorated reader
 	 */
-	public LimitedReader(Reader underlyingReader, long limit) {
+	public LimitedReader(Reader deoratedReader, long limit) {
 		super();
-		this.underlyingReader = underlyingReader;
-		this.limit = limit;
+		if (deoratedReader == null) {
+			throw new NullPointerException("The decorated reader must not be null.");
+		}
+		else if (limit < 0) {
+			throw new IllegalArgumentException("The read limit must not be lower than 0, but was " + limit + ".");
+		}
+		else {
+			this.decoratedReader = deoratedReader;
+			this.limit = limit;
+		}
 	}
 
 
 	/**
-	 * Returns the maximum number of characters that will be read from the decorated reader. Note that this reader will return
-	 *        at most {@code getLimit() - 1} characters.
+	 * Returns the maximum number of characters that will be read from the decorated reader.
 	 * 
 	 * @return the maximum number of characters to be read from the decorated reader
 	 */
@@ -75,33 +78,40 @@ public class LimitedReader extends Reader {
 	
 	/**
 	 * The number of characters available, before the specified maximum number of characters will have been read from the decorated
-	 * reader. Note that the number of characters that can actually be read may be lower, if the decorated reader provides less 
-	 * characters than the specified read limit.
-	 * <p>
-	 * In a new instance of this class the return value of this method will be {@link #getLimit()}{@code - 1}.
+	 * reader.
 	 * 
 	 * @return the number of characters that 
 	 */
 	public long availableCharacters() {
-		return limit - position - 1;
+		return limit - position;
 	}
 	
 	
+	/**
+	 * Checks whether the specified maximum number of characters has been read from the underlying reader.
+	 * 
+	 * @return {@code true} if the maximum number of characters was read
+	 */
 	public boolean isLimitReached() {
 		return availableCharacters() == 0;
 	}
 	
 	
+	/**
+	 * Closes the decorated reader.
+	 * 
+	 * @see java.io.Reader#close()
+	 */
 	@Override
 	public void close() throws IOException {
-		underlyingReader.close();
+		decoratedReader.close();
 	}
 
 
 	@Override
 	public int read(char[] buffer, int offset, int length) throws IOException {
 		length = (int)Math.min(length, availableCharacters());  // The minimum will always be lower than Integer.MIN_VALUE.
-		int result = underlyingReader.read(buffer, offset, length);
+		int result = decoratedReader.read(buffer, offset, length);
 		position += result;
 		return result;
 	}
@@ -110,7 +120,7 @@ public class LimitedReader extends Reader {
 	@Override
 	public long skip(long n) throws IOException {  // This method needs to be overridden because its super implementation would call read() in an endless loop, when the limit is reached.
 		n = Math.min(n, availableCharacters());
-		long result = underlyingReader.skip(n);
+		long result = decoratedReader.skip(n);
 		position += result;
 		return result;
 	}
@@ -120,25 +130,25 @@ public class LimitedReader extends Reader {
 	public synchronized void mark(int readAheadLimit) throws IOException {
 		readAheadLimit = (int)Math.min(readAheadLimit, availableCharacters());  // Buffering more than the read limit is unnecessary.
 		markPosition = position;
-		underlyingReader.mark(readAheadLimit);
+		decoratedReader.mark(readAheadLimit);
 	}
 
 
 	@Override
 	public boolean markSupported() {
-		return underlyingReader.markSupported();
+		return decoratedReader.markSupported();
 	}
 
 
 	@Override
 	public boolean ready() throws IOException {
-		return !isLimitReached() && underlyingReader.ready();
+		return !isLimitReached() && decoratedReader.ready();
 	}
 
 
 	@Override
 	public synchronized void reset() throws IOException {
-		underlyingReader.reset();
-		position = markPosition;  // Will remain unchanged, if the underlying reader thrown a exception.
+		decoratedReader.reset();
+		position = markPosition;  // Will remain unchanged, if the underlying reader would throw an exception.
 	}
 }
