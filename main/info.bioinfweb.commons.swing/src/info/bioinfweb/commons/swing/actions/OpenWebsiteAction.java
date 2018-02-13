432ddf819ba6f628dbd4703c87636d309896299b
BenStoever <stoever@bioinfweb.info>
2018-02-13 14:07:50 +0000

    Source headings corrected.

--- main/info.bioinfweb.commons.swing/src/info/bioinfweb/commons/swing/actions/OpenWebsiteAction.java
+++ main/info.bioinfweb.commons.swing/src/info/bioinfweb/commons/swing/actions/OpenWebsiteAction.java
@@ -1,19 +1,19 @@
 /*
- * TreeGraph 2 - A feature rich editor for phylogenetic trees
- * Copyright (C) 2007-2011, 2013-2017  Ben Stöver, Sarah Wiechers, Kai Müller
- * <http://treegraph.bioinfweb.info/>
+ * bioinfweb.commons.java - Shared components of bioinfweb projects made available in a Java library
+ * Copyright (C) 2008-2011, 2013-2018 Ben Stöver, Sarah Wiechers
+ * <http://commons.bioinfweb.info/Java>
  * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * This program is distributed in the hope that it will be useful,
+ * This file is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package info.bioinfweb.commons.swing.actions;
