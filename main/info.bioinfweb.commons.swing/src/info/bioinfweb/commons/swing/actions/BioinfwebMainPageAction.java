65e85a857408431cc5154520ef11ef5ce87bafbc
BenStoever <stoever@bioinfweb.info>
2018-02-13 14:06:59 +0000

    OpenWebsiteAction moved to actions package. BioinfwebMainPageAction and TwitterAction added. Social media and bioinfweb icons added.

---
+++ main/info.bioinfweb.commons.swing/src/info/bioinfweb/commons/swing/actions/BioinfwebMainPageAction.java
@@ -0,0 +1,36 @@
+/*
+ * AlignmentComparator - An application to efficiently visualize and annotate differences between alternative multiple sequence alignments
+ * Copyright (C) 2014-2018  Ben Stöver
+ * <http://bioinfweb.info/AlignmentComparator>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.bioinfweb.commons.swing.actions;
+
+
+import java.awt.event.KeyEvent;
+
+import javax.swing.Action;
+
+
+
+public class BioinfwebMainPageAction extends OpenWebsiteAction {
+	public BioinfwebMainPageAction() {
+		super("http://bioinfweb.info/");
+		
+		putValue(Action.NAME, "bioinfweb main page"); 
+	  putValue(Action.MNEMONIC_KEY, KeyEvent.VK_B);
+	  loadSymbols("bioinfweb");
+	}
+}
--- main/info.bioinfweb.commons.swing/src/info/bioinfweb/commons/swing/actions/OpenWebsiteAction.java
+++ main/info.bioinfweb.commons.swing/src/info/bioinfweb/commons/swing/actions/OpenWebsiteAction.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package info.bioinfweb.commons.swing;
+package info.bioinfweb.commons.swing.actions;
 
 
 import java.awt.Desktop;
@@ -26,6 +26,8 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import javax.swing.JOptionPane;
+
+import info.bioinfweb.commons.swing.ExtendedAbstractAction;
 
 
 
@@ -49,17 +51,50 @@ public class OpenWebsiteAction extends ExtendedAbstractAction {
 	}
 	
 	
+	/**
+	 * Creates a new instance of this class.
+	 * 
+	 * @param url the URL to be opened
+	 * @param pathPrefix the prefix of the resource path to load icons from
+	 */
+	public OpenWebsiteAction(String url, String pathPrefix) {
+		this(createURI(url), pathPrefix);
+	}
+	
+	
+	/**
+	 * Creates a new instance of this class.
+	 * 
+	 * @param url the URL to be opened
+	 */
 	public OpenWebsiteAction(String url) {
 		this(createURI(url));
 	}
 	
 	
+	/**
+	 * Creates a new instance of this class.
+	 * 
+	 * @param url the URL to be opened
+	 * @param pathPrefix the prefix of the resource path to load icons from
+	 */
+	public OpenWebsiteAction(URI url, String pathPrefix) {
+		super(pathPrefix);
+		this.url = url;
+	}
+
+	
+	/**
+	 * Creates a new instance of this class.
+	 * 
+	 * @param url the URL to be opened
+	 */
 	public OpenWebsiteAction(URI url) {
 		super();
 		this.url = url;
 	}
 
-
+	
 	public URI getURL() {
 		return url;
 	}
