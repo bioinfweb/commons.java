a40722f9dcb70c37aab82862222c75456b355c4f
BenStoever <stoever@bioinfweb.info>
2020-10-16 13:44:09 +0000

    Minor JavaDoc changes.

--- main/info.bioinfweb.commons.swing/src/info/bioinfweb/commons/swing/TableColumnModelAdapter.java
+++ main/info.bioinfweb.commons.swing/src/info/bioinfweb/commons/swing/TableColumnModelAdapter.java
@@ -23,48 +23,35 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.TableColumnModelEvent;
 import javax.swing.event.TableColumnModelListener;
+import javax.swing.table.TableColumnModel;
 
 
 
 /**
- * <p>An abstract adapter class for receiving events of a <code>TableColumnModel</code>. 
+ * <An abstract adapter class for receiving events of a {@link TableColumnModel}. 
  * The methods in this class are empty. This class exists as convenience for creating 
- * listener objects.</p>
- * <p>Extend this class to create a <code>TableColumnModelListener</code> listener and 
+ * listener objects.
+ * <p>
+ * Extend this class to create a {@link TableColumnModelListener} listener and 
  * override the methods for the events of interest. (If you implement the 
- * <code>TableColumnModelListener</code> interface, you have to define all of the 
+ * {@link TableColumnModelListener} interface, you have to define all of the 
  * methods in it. This abstract class defines null methods for them all, so 
- * you can only have to define methods for events you care about.)</p> 
- * @author BenStoever
- *
+ * you can only have to define methods for events you care about.)
+ *  
+ * @author Ben St&ouml;ver
  */
 public abstract class TableColumnModelAdapter implements TableColumnModelListener {
-	/* (non-Javadoc)
-	 * @see javax.swing.event.TableColumnModelListener#columnAdded(javax.swing.event.TableColumnModelEvent)
-	 */
 	public void columnAdded(TableColumnModelEvent e) {}
 
 	
-	/* (non-Javadoc)
-	 * @see javax.swing.event.TableColumnModelListener#columnMarginChanged(javax.swing.event.ChangeEvent)
-	 */
 	public void columnMarginChanged(ChangeEvent e) {}
 
 	
-	/* (non-Javadoc)
-	 * @see javax.swing.event.TableColumnModelListener#columnMoved(javax.swing.event.TableColumnModelEvent)
-	 */
 	public void columnMoved(TableColumnModelEvent e) {}
 
 	
-	/* (non-Javadoc)
-	 * @see javax.swing.event.TableColumnModelListener#columnRemoved(javax.swing.event.TableColumnModelEvent)
-	 */
 	public void columnRemoved(TableColumnModelEvent e) {}
 
 	
-	/* (non-Javadoc)
-	 * @see javax.swing.event.TableColumnModelListener#columnSelectionChanged(javax.swing.event.ListSelectionEvent)
-	 */
 	public void columnSelectionChanged(ListSelectionEvent e) {}
 }
